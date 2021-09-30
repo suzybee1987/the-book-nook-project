e69776977365d1e418908636a3341456e1461df8
suzybee1987 <suzannemiller1987@gmail.com>
2021-09-30 07:16:28 +0000

    Dev: add login functionality

--- app.py
+++ app.py
@@ -57,6 +57,8 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration successful, {}".format(
             request.form.get("username")))
+        return redirect(url_for(
+                "profile", name=session["user"]))
 
     return render_template("register.html")
 
@@ -70,9 +72,13 @@ def login():
 
         if existing_user:
             # ensure hashed password matches user input
-            if check_password_hash(existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session['user'] = request.form.get('username').lower()
+                flash("Welcome, {}".format(
+                    request.form.get('username')))
+                return redirect(url_for(
+                    "profile", username=session['user']))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -84,6 +90,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
