701a159532786ea25c34f096d819ce6e23bf75a2
suzybee1987 <suzannemiller1987@gmail.com>
2021-10-04 08:50:35 +0000

    Dev: Styled wallpapers to full screen

--- app.py
+++ app.py
@@ -208,6 +208,12 @@ def delete_review(review_id):
     return redirect(url_for('get_reviews'))
 
 
+@app.route("/get_genres")
+def get_genres():
+    genres = list(mongo.db.genres.find().sort("genre_name", 1))
+    return render_template("genres.html", genres=genres)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
